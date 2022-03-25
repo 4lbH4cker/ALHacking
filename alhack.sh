c3365f51db87b6b62c7b826970f7e276793e5381
Aurel <94483858+ahgaurel1@users.noreply.github.com>
2022-03-25 13:30:29 +0200

    Add files via upload

---
+++ alhack.sh
@@ -0,0 +1,202 @@
+clear
+mkdir Tools
+clear 
+echo -e '\033[31;40;1m 
+  █████╗ ██╗             ████████╗ ██████╗  ██████╗ ██╗
+ ██╔══██╗██║             ╚══██╔══╝██╔═══██╗██╔═══██╗██║
+ ███████║██║     ███████╗   ██║   ██║   ██║██║   ██║██║
+ ██╔══██║██║     ╚══════╝   ██║   ██║   ██║██║   ██║██║
+ ██║  ██║███████╗           ██║   ╚██████╔╝╚██████╔╝███████╗
+ ╚═╝  ╚═╝╚══════╝           ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝ v3
+  Coded by ahgaurel1
+  ig: _.aurel_vllahi
+  github: https://github.com/ahgaurel1
+\033[33;4mVersion:\033[0m 3            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m ahgaurel1
+
+\e[37m[1]\e[36m Kërkesat & Përditësimi        \e[37m[2]\e[36m Phishing Tool				
+\e[37m[3]\e[36m WebCam Hack                   \e[37m[4]\e[36m User Find					
+\e[37m[5]\e[36m Gmail Bomber		  \e[37m[6]\e[36m DDOS Attack			
+\e[37m[7]\e[36m Si të përdorni ?	          \e[37m[8]\e[36m Çinstaloni programet e shkarkuara		
+\e[37m[9]\e[36m Ip Info	                  \e[37m[10]\e[36m dorks-eye
+\e[37m[11]\e[36m ghost_eye                    \e[37m[12]\e[36m RED_HAWK
+\e[37m[13]\e[36m VirusCrafter                 \e[37m[14]\e[36m Info-Site
+\e[37m[15]\e[36m BadMod	                  \e[37m[16]\e[36m Facebash
+'
+
+
+#Zgjedhja e Opsionit
+
+
+read -p "Numri i transaksionit: " islem
+if [[ $islem == 1 || $islem == 01 ]]; then
+clear
+
+echo -e "\033[47;31;5m Po instalon përditësimin dhe kërkesat...\033[0m"
+sleep 5
+pkg install git -y
+pkg install python python2 -y
+pkg install pip pip2 -y
+pkg install curl -y
+apt update
+apt upgrade -y
+clear
+echo -e "\033[47;3;35m Përditësimi i plotë...\033[0m"
+sleep 3
+bash tga.sh
+
+elif [[ $islem == 2 || $islem == 02 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/htr-tech/zphisher
+cd zphisher
+bash zphisher.sh
+
+elif [[ $islem == 3 || $islem == 03 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/techchipnet/CamPhish
+cd CamPhish
+bash camphish.sh
+
+elif [[ $islem == 4 || $islem == 04 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/xHak9x/finduser
+cd finduser
+bash finduser.sh
+       
+elif [[ $islem == 5 || $islem == 05 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/palahsu/MBomb.git
+cd MBomb
+python MBomb.py
+
+elif [[ $islem == 6 || $islem == 06 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+pip3 install requests pysocks
+git clone https://github.com/Leeon123/CC-attack
+cd CC-attack
+python3 cc.py
+
+elif [[ $islem == 7 || $islem == 07 ]]; then
+clear
+xdg-open https://pasteio.com/xuCvIkXdNRIB
+bash tga.sh
+
+ 
+elif [[ $islem == 8 ]]; then
+        clear
+
+echo -e "\033[47;3;35m PO HIQEN PROGRAMET E SHKARKUARA...\033[0m"
+sleep 3 
+rm -rf Tools
+
+bash tga.sh
+
+elif [[ $islem == 9 || $islem == 09 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/ahgaurel1/ipinfo
+cd ipinfo
+bash install.sh
+python3 ipinfo.py
+
+elif [[ $islem == 10 || $islem == 010 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/BullsEye0/dorks-eye.git
+cd dorks-eye
+pip install -r requirements.txt
+python3 dorks-eye.py
+
+elif [[ $islem == 11 || $islem == 011 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/BullsEye0/ghost_eye.git
+cd ghost_eye
+pip install -r requirements.txt
+python3 ghost_eye.py
+
+elif [[ $islem == 12 || $islem == 012 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+
+elif [[ $islem == 13 || $islem == 013 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/Devil-Tigers/TigerVirus
+apt update
+apt upgrade -y
+pkg install git -y
+cd TigerVirus
+bash TigerVirus.sh
+
+elif [[ $islem == 14 || $islem == 014 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+pkg install curl -y
+upgrade -y
+pkg install git -y
+git clone https://github.com/king-hacking/info-site.git
+cd info-site
+bash info.sh
+
+elif [[ $islem == 15 || $islem == 015 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+sudo apt-get update
+sudo apt-get install php
+sudo apt-get install php-curl
+git clone https://github.com/MrSqar-Ye/BadMod.git
+cd BadMod
+chmod u+x INSTALL
+chmod u+x BadMod.php
+sudo php BadMod.php
+
+elif [[ $islem == 16 || $islem == 016 ]]; then
+clear
+echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+sleep 3
+cd Tools
+git clone https://github.com/fu8uk1/facebash
+cd facebash
+chmod +x facebash.sh
+service tor start
+sudo ./facebash.sh
+
+else   
+	clear
+        echo -e '\033[36;40;1m Keni futur kodin e gabuar'	
+	sleep 1
+	clear 
+	bash tga.sh
+fi
