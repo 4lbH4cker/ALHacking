c666b0234b59ddf2453afe4760644fa2c8e13d15
mr Aurel <94483858+mrAurel@users.noreply.github.com>
2022-03-25 17:14:29 +0200

    Update alhack.sh

--- alhack.sh
+++ alhack.sh
@@ -8,10 +8,10 @@ echo -e '\033[31;40;1m
  ██╔══██║██║     ╚══════╝   ██║   ██║   ██║██║   ██║██║
  ██║  ██║███████╗           ██║   ╚██████╔╝╚██████╔╝███████╗
  ╚═╝  ╚═╝╚══════╝           ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝ v3
-  Coded by ahgaurel1
+  Coded by mrAurel
   ig: _.aurel_vllahi
-  github: https://github.com/ahgaurel1
-\033[33;4mVersion:\033[0m 3            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m ahgaurel1
+  github: https://github.com/mrAurel
+\033[33;4mVersion:\033[0m 3            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m mrAurel
 
 \e[37m[1]\e[36m Kërkesat & Përditësimi        \e[37m[2]\e[36m Phishing Tool				
 \e[37m[3]\e[36m WebCam Hack                   \e[37m[4]\e[36m User Find					
