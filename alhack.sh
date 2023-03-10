ccb25e6d3af27bd2499dd6cc43f676d0f96d27be
4lbH4cker <94483858+4lbH4cker@users.noreply.github.com>
2023-03-10 18:55:33 +0200

    Update alhack.sh

--- alhack.sh
+++ alhack.sh
@@ -10,7 +10,7 @@ echo -e '\033[31;40;1m
  ╚═╝  ╚═╝╚══════╝           ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝ v3
   Coded by 4lbH4cker
   github: https://github.com/4lbH4cker
-\033[33;4mVersion:\033[0m 3            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m mrAurel
+\033[33;4mVersion:\033[0m 3            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m 4lbH4cker
 
 \e[37m[1]\e[36m Kërkesat & Përditësimi        \e[37m[2]\e[36m Phishing Tool				
 \e[37m[3]\e[36m WebCam Hack                   \e[37m[4]\e[36m User Find					
